e0c2f4e0290fd48cec814915843449f38a1d2639
aayafrontenda <eeaayeyeah@gmail.com>
2023-06-25 14:31:18 +0600

    feat: translations

--- pages/cleaner/index.tsx
+++ pages/cleaner/index.tsx
@@ -1,6 +1,72 @@
+// import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+
+import { Work_Sans } from "@next/font/google";
+
+/* import clsx from "clsx";
+import { observer } from "mobx-react-lite";
+import type { NextPage } from "next";
+// import "./styles/Home.module.css";
+import { useRouter } from "next/router";
+
+import { AuthApi } from "@/api";
+import { InputText } from "@/components/molecules/InputText";
+import { LanguagePopover } from "@/components/molecules/LanguagesPopover";
+import { User } from "@/store/User/User";
+*/
+
+const workSans = Work_Sans({
+  subsets: ["latin"],
+});
+
 const Cleaner = () => {
   console.log("cleaner page");
-  return <div>Cleaner page</div>;
+  const { t } = useTranslation();
+  return (
+    <div className={workSans.className}>
+      <header className="justify-center p-10 font-medium text-[#292929]">
+        <nav>
+          <ul className="flex list-none items-center justify-center gap-12 px-32">
+            <li>{t("indexPage.home")}</li>
+            <li>{t("cleanerPage.orders")}</li>
+            <li className="mr-auto">{t("indexPage.contacts")}</li>
+            <button className="flex w-[9rem] cursor-pointer items-center justify-center rounded-3xl border-none bg-[#1195FF] p-3 text-white outline-none hover:bg-[#1195FF]/80">
+              Log out
+            </button>
+          </ul>
+        </nav>
+      </header>
+      <main className="flex h-screen flex-col justify-start">
+        <div className="flex flex-col px-32">
+          <img
+            className="mr-auto h-[4rem] w-[4rem] shrink-0"
+            src="../apartXlogo.svg"
+            alt="apart-x-logo"
+          />
+          <p>
+            Look for the orders in <u className="cursor-pointer">Almaty</u>{" "}
+            today
+          </p>
+        </div>
+        <div className="flex flex-col px-32">
+          <div
+            className="h-[279px] w-[849px] rounded-lg"
+            style={{
+              backgroundImage: "url(../order-1.png)",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <div className="flex justify-between p-4 text-white">
+              <div className="flex flex-col text-[24px] font-bold">
+                <p className="m-1 p-1">Ерканат</p>
+                <img src="../stars.svg" alt="star" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Cleaner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -36,47 +36,47 @@ interface PortfolioElementProps {
   picSource: string;
 }
 
-const portfolio: PortfolioElementProps[] = [
-  {
-    description: "Professional Cleaner",
-    isActive: false,
-    name: "Corporate Vehicle Cleaning",
-    picSource: "../Rectangle 37.png",
-  },
-  {
-    description: "Professional Cleaner",
-    isActive: false,
-    name: "Corporate Vehicle Cleaning",
-    picSource: "../Rectangle 38.png",
-  },
-  {
-    description: "Professional Cleaner",
-    isActive: true,
-    name: "Corporate Vehicle Cleaning",
-    picSource: "../Rectangle 39.png",
-  },
-  {
-    description: "Professional Cleaner",
-    isActive: false,
-    name: "Corporate Vehicle Cleaning",
-    picSource: "../Rectangle 37.png",
-  },
-  {
-    description: "Professional Cleaner",
-    isActive: false,
-    name: "Corporate Vehicle Cleaning",
-    picSource: "../Rectangle 38.png",
-  },
-];
-
 const PortfolioSlider: React.FC = () => {
+  const { t } = useTranslation();
+  const portfolio: PortfolioElementProps[] = [
+    {
+      description: t("indexPage.sliderMembers.windowCleanerDescription"),
+      isActive: false,
+      name: t("indexPage.sliderMembers.windowCleaner"),
+      picSource: "../Rectangle 37.png",
+    },
+    {
+      description: t("indexPage.sliderMembers.houseCleanerDescription"),
+      isActive: false,
+      name: t("indexPage.sliderMembers.windowCleaner"),
+      picSource: "../Rectangle 38.png",
+    },
+    {
+      description: t("indexPage.sliderMembers.commercialCleanerDescription"),
+      isActive: true,
+      name: t("indexPage.sliderMembers.commercialCleaner"),
+      picSource: "../Rectangle 39.png",
+    },
+    {
+      description: t("indexPage.sliderMembers.floorCleanerDescription"),
+      isActive: false,
+      name: t("indexPage.sliderMembers.floorCleaner"),
+      picSource: "../Rectangle 37.png",
+    },
+    {
+      description: t("indexPage.sliderMembers.floorCleanerDescription"),
+      isActive: false,
+      name: t("indexPage.sliderMembers.carpetCleaner"),
+      picSource: "../Rectangle 38.png",
+    },
+  ];
   const [index, setIndex] = useState(0);
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((index) => (index + 1) % portfolio.length);
     }, 5000);
     return () => clearInterval(interval);
-  }, []);
+  }, [portfolio.length]);
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
@@ -233,6 +233,7 @@ const Member: React.FC<MemberProps> = ({ name, position, picSource }) => {
 
 const RoomSelector: React.FC = () => {
   const [rooms, setRooms] = useState(1);
+  const { t } = useTranslation();
 
   return (
     <div className="flex h-8 items-center justify-center gap-2 rounded bg-white text-white">
@@ -244,7 +245,7 @@ const RoomSelector: React.FC = () => {
         -
       </button>
       <p className="m-0 flex items-center p-0 text-[#000000]">
-        {rooms}-комнатная
+        {t(`indexPage.rooms.${rooms}`)}
       </p>
       <button
         className="h-8 w-8 cursor-pointer rounded-xl rounded-r border-none bg-[#1195FF] outline-none hover:bg-[#1195FF]/80"
@@ -259,13 +260,7 @@ const RoomSelector: React.FC = () => {
 
 const BathroomSelector: React.FC = () => {
   const [rooms, setRooms] = useState(1);
-  const RUS_LOCALE_BATHROOMS_COUNT: Record<number, string> = {
-    1: "санузел",
-    2: "санузла",
-    3: "санузла",
-    4: "санузла",
-    5: "санузлов",
-  };
+  const { t } = useTranslation();
 
   return (
     <div className="flex h-8 items-center justify-between gap-4 rounded bg-white text-white">
@@ -277,7 +272,7 @@ const BathroomSelector: React.FC = () => {
         -
       </button>
       <p className="m-0 flex items-center p-0 text-[#000000]">
-        {rooms} {RUS_LOCALE_BATHROOMS_COUNT[rooms]}
+        {t(`indexPage.bathRooms.${rooms}`)}
       </p>
       <button
         className="h-8 w-8 cursor-pointer rounded-xl rounded-r border-none bg-[#1195FF] outline-none hover:bg-[#1195FF]/80"
@@ -333,10 +328,10 @@ const Home: NextPage = observer(() => {
               />
             </li>
             <li>{t("indexPage.home")}</li>
-            <li>ABOUT US</li>
-            <li>SERVICES</li>
-            <li>BLOG</li>
-            <li className="">CONTACT</li>
+            <li>{t("indexPage.aboutUs")}</li>
+            <li>{t("indexPage.services")}</li>
+            <li>{t("indexPage.blog")}</li>
+            <li className="">{t("indexPage.contacts")}</li>
             {User.auth ? (
               <li
                 className="cursor-pointer"
@@ -360,7 +355,7 @@ const Home: NextPage = observer(() => {
                   }}
                   className="flex w-[9rem] cursor-pointer items-center justify-center rounded-3xl border-none bg-[#1195FF] p-3 text-white outline-none hover:bg-[#1195FF]/80"
                 >
-                  Log in
+                  {t("loginPage.logIn")}
                 </button>
               </li>
             )}
@@ -374,21 +369,25 @@ const Home: NextPage = observer(() => {
             className="mt-16 flex w-1/2 flex-col py-10 text-left"
             id="description"
           >
-            <p>HIGHLY PROFESSIONAL CLEANING</p>
+            <p>{t("indexPage.information1.title1")}</p>
             <h1 className="flex flex-col">
-              <span className="font-bold text-[#1195FF]">EASY TO CLEAN</span>
-              <span className="font-bold text-[#555555]">HOUSE AND OFFICE</span>
+              <span className="font-bold text-[#1195FF]">
+                {t("indexPage.information1.title2")}
+              </span>
+              <span className="font-bold text-[#555555]">
+                {t("indexPage.information1.title3")}
+              </span>
             </h1>
-            <p>
-              Welcome to ApartX, your go-to cleaning service! Our skilled and
-              passionate team is dedicated to delivering exceptional cleaning
-              services tailored to your unique needs.
-            </p>
+            <p>{t("indexPage.information1.welcomeText")}</p>
             <button className="flex w-[9rem] cursor-pointer items-center justify-center rounded-3xl border-none bg-[#1195FF] p-3 text-white outline-none  hover:bg-[#1195FF]/80">
-              Get a Quote
+              {t("indexPage.information1.getAQuote")}
             </button>
-            <p className="pt-20 text-[#1195FF]">TRUSTED PARTNERS</p>
-            <p className="font-bold text-[#555555]">BRANDS WHO TRUST US</p>
+            <p className="pt-20 text-[#1195FF]">
+              {t("indexPage.information1.trustedPartners")}
+            </p>
+            <p className="font-bold text-[#555555]">
+              {t("indexPage.information1.trustedBrands")}
+            </p>
           </div>
           <img
             alt="cleaner"
@@ -411,7 +410,7 @@ const Home: NextPage = observer(() => {
             }}
           />
           <button className="flex w-full cursor-pointer items-center justify-center rounded border-none bg-[#1195FF] p-3 text-white outline-none hover:bg-[#1195FF]/80">
-            Рассчитать стоимость
+            {t("indexPage.calculatePrice")}
           </button>
         </div>
         {/* LINE WITH LOGOS */}
@@ -430,31 +429,24 @@ const Home: NextPage = observer(() => {
             className="mt-16 flex w-2/3 flex-col py-10 text-left"
             id="description"
           >
-            <p className="pt-20 text-[#1195FF]">ABOUT US</p>
-            <p>HIGHLY PROFESSIONAL CLEANING</p>
+            <p className="pt-20 text-[#1195FF]">{t("indexPage.aboutUs")}</p>
+            <p>{t("indexPage.information1.title1")}</p>
             <h1 className="flex flex-col">
               <span className="font-bold text-[#555555]">
-                QUICKLY AND EASY TO CLEAN
+                {t("indexPage.information2.title1")}
               </span>
               <span className="font-bold text-[#555555]">
-                YOUR HOUSE AND OFFICE
+                {t("indexPage.information1.title3")}
               </span>
             </h1>
-            <p className="w-2/3">
-              Amet minim mollit non deserunt ullamco sit aliqua dolor do
-              consequat sunt nostrud amet. Amet minim mollit non deserunt
-              ullamco aliqua dolo ame.
-            </p>
+            <p className="w-2/3">{t("indexPage.information2.text1")}</p>
             <div className="mt-4 flex items-center gap-1">
-              AWARD WINNING COMPANY
+              {t("indexPage.information2.title2")}
               <img alt="award" src="../award.svg" />
             </div>
-            <p className="w-2/3">
-              Amet minim mollit non sit aliqua dolor do consequat sunt nostrud
-              amet. Amet mollit ullamco aliqua dolo ame.
-            </p>
+            <p className="w-2/3">{t("indexPage.information2.text2")}</p>
             <button className="flex w-[8rem] cursor-pointer items-center justify-center rounded-3xl border-none bg-[#1195FF] p-3 text-white outline-none  hover:bg-[#1195FF]/80">
-              Our Services
+              {t("indexPage.information2.ourServices")}
             </button>
           </div>
           <div className="flex gap-4">
@@ -477,33 +469,39 @@ const Home: NextPage = observer(() => {
 
         {/* OUR TEAM OFFERS */}
         <div className="flex flex-col  items-center justify-center gap-4 px-32">
-          <p className="text-[#1195FF]">OUR TEAM</p>
+          <p className="text-[#1195FF]">
+            {t("indexPage.ourTeamOffers.ourTeam")}
+          </p>
           <h2 className="mb-12 font-bold text-[#555555]">
-            WHAT WE ARE OFFERING
+            {t("indexPage.ourTeamOffers.offering")}
           </h2>
           <div className="flex gap-4">
             <Offer
-              name="House Cleaning"
-              description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint."
+              name={t("indexPage.ourTeamOffers.services.houseCleaning")}
+              description={t(
+                "indexPage.ourTeamOffers.descriptions.houseCleaning"
+              )}
               picSource="../group-38.svg"
             />
             <Offer
-              name="Office Cleaning"
-              description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint."
+              name={t("indexPage.ourTeamOffers.services.officeCleaning")}
+              description={t(
+                "indexPage.ourTeamOffers.descriptions.officeCleaning"
+              )}
               picSource="../group-35.svg"
             />
             <Offer
-              name="Floor Cleaning"
-              description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint."
+              name={t("indexPage.ourTeamOffers.services.floorCleaning")}
+              description={t(
+                "indexPage.ourTeamOffers.descriptions.floorCleaning"
+              )}
               picSource="../group-37.svg"
             />
             <Offer
-              name="Window Cleaning"
-              description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-              amet sint."
+              name={t("indexPage.ourTeamOffers.services.windowCleaning")}
+              description={t(
+                "indexPage.ourTeamOffers.descriptions.windowCleaning"
+              )}
               picSource="../group-36.svg"
             />
           </div>
@@ -511,29 +509,31 @@ const Home: NextPage = observer(() => {
 
         {/* OUR TEAM MEMBERS */}
         <div className="flex flex-col  items-center justify-center gap-4 px-32">
-          <p className="mt-16 text-[#1195FF]">OUR TEAM</p>
+          <p className="mt-16 text-[#1195FF]">
+            {t("indexPage.ourTeamOffers.ourTeam")}
+          </p>
           <h2 className="mb-2 font-bold text-[#555555]">
-            OUR EXPERT TEAM MEMBERS
+            {t("indexPage.ourTeamMembers.experts")}
           </h2>
           <div className="flex gap-4">
             <Member
               name="Devon Lane"
-              position="Window Cleaner"
+              position={t("indexPage.sliderMembers.windowCleaner")}
               picSource="../member-3.png"
             />
             <Member
               name="Cody Phisher"
-              position="House Cleaner"
+              position={t("indexPage.sliderMembers.houseCleaner")}
               picSource="../member-4.png"
             />
             <Member
               name="Ralph Edwards"
-              position="Commercial Cleaner"
+              position={t("indexPage.sliderMembers.commercialCleaner")}
               picSource="../member-2.png"
             />
             <Member
               name="Bessie Cooper"
-              position="Floor Cleaner"
+              position={t("indexPage.sliderMembers.floorCleaner")}
               picSource="../member-1.png"
             />
           </div>
@@ -541,9 +541,11 @@ const Home: NextPage = observer(() => {
 
         {/* OUR PORTFOLIO */}
         <div className="flex flex-col items-center justify-center gap-4">
-          <p className="mt-16 text-[#1195FF]">OUR PORTFOLIO</p>
+          <p className="mt-16 text-[#1195FF]">
+            {t("indexPage.portfolio.title")}
+          </p>
           <h2 className="mb-2 font-bold text-[#555555]">
-            RECENT WORK SHOWCASE
+            {t("indexPage.portfolio.showcase")}
           </h2>
           <PortfolioSlider />
         </div>
@@ -554,7 +556,7 @@ const Home: NextPage = observer(() => {
               <div className="flex-col items-center text-white">
                 <h1 className="m-0 p-0 text-[24.5px] font-bold">567+</h1>
                 <p className="m-0 whitespace-nowrap p-0 text-[12px]">
-                  Happy Customers
+                  {t("indexPage.portfolio.happyCustomers")}
                 </p>
               </div>
             </div>
@@ -564,7 +566,7 @@ const Home: NextPage = observer(() => {
               <div className="flex-col items-center text-white">
                 <h1 className="m-0 p-0 text-[24.5px] font-bold">48</h1>
                 <p className="m-0 whitespace-nowrap p-0 text-[12px]">
-                  Team Members
+                  {t("indexPage.portfolio.teamMembers")}
                 </p>
               </div>
             </div>
@@ -574,7 +576,7 @@ const Home: NextPage = observer(() => {
               <div className="flex-col items-center text-white">
                 <h1 className="m-0 p-0 text-[24.5px] font-bold">26</h1>
                 <p className="m-0 whitespace-nowrap p-0 text-[12px]">
-                  Award Winning
+                  {t("indexPage.portfolio.awardsWinning")}
                 </p>
               </div>
             </div>
@@ -584,7 +586,7 @@ const Home: NextPage = observer(() => {
               <div className="flex-col items-center text-white">
                 <h1 className="m-0 p-0 text-[24.5px] font-bold">759+</h1>
                 <p className="m-0 whitespace-nowrap p-0 text-[12px]">
-                  Projects Complete
+                  {t("indexPage.portfolio.projectsComplete")}
                 </p>
               </div>
             </div>
